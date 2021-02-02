064299064863414a768e766202022457ec15c7ec
Dejuan4114 <dejuan.whipple@patelhs.org>
2021-02-02 14:02:12 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args)
+  {
+ 
+  }
+  static int (int num1, int num2)
+  {
+      return num1 + num2;
+  }
+
+
+
+  static boolean weather()
+    {
+
+    }
+ 
+  
+  }
+}
