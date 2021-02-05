20627fe41f84ecccbeef988470b38e67c1ee439a
Dejuan4114 <dejuan.whipple@patelhs.org>
2021-02-05 17:39:46 +0000

    I had to fix the repl because I wasn't calling the method correctly.

--- Main.java
+++ Main.java
@@ -1,20 +1,20 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args)
-  {
- 
-  }
-  static int (int num1, int num2)
-  {
-      return num1 + num2;
-  }
+ {
+   Scanner scan = new Scanner(System.in);
 
-
-
-  static boolean weather()
-    {
-
-    }
- 
-  
-  }
+   System.out.println("How much do the pants cost?");
+   int clothes = scan.nextInt();
+   System.out.println(inventory(clothes));
+ }
+ static double inventory(double pantsAmount,double dayAmount,int totalAmount)
+ { 
+ double amountLeft = dayAmount - totalAmount ;
+ return amountLeft;
+ }
+//Create a second method and call your method within the main method
+{
+  static int 
 }
+  }
